7f5e3879728f6e9c1d817a6d72c6e81eeb4d8c36
Kevin Grant <kevincgrant.kg@gmail.com>
2017-10-22 16:02:47 -0700

    leaders card polish

--- app/src/main/java/com/example/kevin/fifastatistics/views/databinding/ImageBindingAdapter.java
+++ app/src/main/java/com/example/kevin/fifastatistics/views/databinding/ImageBindingAdapter.java
@@ -4,10 +4,18 @@ import android.databinding.BindingAdapter;
 import android.support.annotation.DrawableRes;
 import android.widget.ImageView;
 
+import com.example.kevin.fifastatistics.R;
 import com.example.kevin.fifastatistics.interfaces.ImageCallback;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class ImageBindingAdapter {
+
+    private static final int DEFAULT_HEADSHOT_RES_ID = R.drawable.circle_diagonal;
+
+    @BindingAdapter(value = "headshotImageUrl")
+    public static void loadHeadshotImage(ImageView view, String imageUrl) {
+        loadImage(view, imageUrl, null, DEFAULT_HEADSHOT_RES_ID);
+    }
 
     @BindingAdapter(value = {"imageUrl", "imageCallback", "defaultResId"}, requireAll = false)
     public static void loadImage(ImageView view, String imageUrl, ImageCallback imageCallback, @DrawableRes int defaultResId) {
