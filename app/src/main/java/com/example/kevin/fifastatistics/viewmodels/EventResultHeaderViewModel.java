03a3d8315df9ffe79b0897db8dd85b2e80119618
Kevin Grant <kevincgrant.kg@gmail.com>
2018-06-21 08:43:12 -0700

    Move message to header

--- app/src/main/java/com/example/kevin/fifastatistics/viewmodels/EventResultHeaderViewModel.java
+++ app/src/main/java/com/example/kevin/fifastatistics/viewmodels/EventResultHeaderViewModel.java
@@ -1,14 +1,20 @@
 package com.example.kevin.fifastatistics.viewmodels;
 
+import android.content.res.Resources;
 import android.databinding.Bindable;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.example.kevin.fifastatistics.FifaApplication;
+import com.example.kevin.fifastatistics.R;
 import com.example.kevin.fifastatistics.managers.CrestUrlResizer;
 import com.example.kevin.fifastatistics.managers.EventPresenter;
 import com.example.kevin.fifastatistics.models.databasemodels.match.FifaEvent;
 import com.example.kevin.fifastatistics.models.databasemodels.match.PenaltyEvent;
+import com.example.kevin.fifastatistics.models.databasemodels.match.Series;
+import com.example.kevin.fifastatistics.models.databasemodels.match.SeriesProjection;
 import com.example.kevin.fifastatistics.models.databasemodels.user.Player;
+import com.example.kevin.fifastatistics.utils.ResourceUtils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -54,6 +60,10 @@ public class EventResultHeaderViewModel extends FifaBaseViewModel {
         if (doesEventHavePenalties()) {
             String penalties = mMatch.getPenalties().toString();
             return String.format(PENALTIES_MESSAGE, getFirstName(mEvent.getWinnerName()), penalties);
+        } else if (mEvent instanceof Series || mEvent instanceof SeriesProjection) {
+            Resources r = FifaApplication.getContext().getResources();
+            int matches = (mEvent.getScoreWinner() * 2) - 1;
+            return r.getString(R.string.best_of, matches);
         } else {
             return null;
         }
@@ -61,7 +71,7 @@ public class EventResultHeaderViewModel extends FifaBaseViewModel {
 
     @Bindable
     public int getMessageVisibility() {
-        return doesEventHavePenalties() ? View.VISIBLE : View.GONE;
+        return mMatch == null || mMatch.getPenalties() != null ? View.VISIBLE : View.GONE;
     }
 
     private boolean doesEventHavePenalties() {
