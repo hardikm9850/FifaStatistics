63bd62d077731e1a4397a2c8f90aac5c95a71680
Kevin Grant <kevincgrant.kg@gmail.com>
2018-07-25 16:04:08 -0700

    Series seekbar improvements

--- app/src/main/java/com/example/kevin/fifastatistics/views/DiscreteNumberPicker.java
+++ app/src/main/java/com/example/kevin/fifastatistics/views/DiscreteNumberPicker.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.databinding.BindingAdapter;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
@@ -13,17 +14,21 @@ import android.widget.TextView;
 import com.example.kevin.fifastatistics.R;
 import com.example.kevin.fifastatistics.listeners.SimpleOnSeekBarChangeListener;
 
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
 public class DiscreteNumberPicker extends LinearLayout {
 
     private static final int MESSAGE_ID = R.string.best_of;
     private static final int DEFAULT_MIN = 0;
     private static final int DEFAULT_MAX = 99;
     private static final int DEFAULT_STEP = 1;
+    private static final int STEP_MULTIPLER = 1000;
 
     private OnNumberChangedListener listener;
     private TextView label;
     private SeekBar seekBar;
-    private String numberText;
+    private NavigableSet<Integer> values;
     private int min;
     private int max;
     private int step;
@@ -44,7 +49,7 @@ public class DiscreteNumberPicker extends LinearLayout {
     public DiscreteNumberPicker(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         View root = initView(context);
-        initAttributes(context, attrs, defStyleAttr, defStyleRes, root);
+        initAttributes(context, attrs, defStyleAttr, defStyleRes);
         initSeekbar(root);
     }
 
@@ -55,16 +60,16 @@ public class DiscreteNumberPicker extends LinearLayout {
         return root;
     }
 
-    private void initAttributes(Context context, AttributeSet attrs, int defStyle, int defStyleRes, View root) {
+    private void initAttributes(Context context, AttributeSet attrs, int defStyle, int defStyleRes) {
         TypedArray ta = context. obtainStyledAttributes(attrs, R.styleable.DiscreteNumberPicker, defStyle, defStyleRes);
         try {
-            min = ta.getInt(R.styleable.DiscreteNumberPicker_min, DEFAULT_MIN);
             max = ta.getInt(R.styleable.DiscreteNumberPicker_max, DEFAULT_MAX);
+            min = ta.getInt(R.styleable.DiscreteNumberPicker_min, DEFAULT_MIN);
             step = ta.getInt(R.styleable.DiscreteNumberPicker_step, DEFAULT_STEP);
             int start = ta.getInt(R.styleable.DiscreteNumberPicker_start, min - 1);
             processStart(start);
             verifyAttributes();
-            numberText = ta.getString(R.styleable.DiscreteNumberPicker_textLabel);
+            initValues();
         } finally {
             ta.recycle();
         }
@@ -82,15 +87,24 @@ public class DiscreteNumberPicker extends LinearLayout {
         }
     }
 
+    private void initValues() {
+        int size = ((max - min)/step) + 1;
+        values = new TreeSet<>();
+        values.add(0);
+        for (int i = 0; i < size; i++) {
+            values.add(i*step*STEP_MULTIPLER);
+        }
+    }
+
     private void initSeekbar(View root) {
         seekBar = root.findViewById(R.id.seekbar);
-        seekBar.setMin(min);
-        seekBar.setMax((int)Math.ceil(((double) (max + min)) / step));
+        seekBar.setMax(max*STEP_MULTIPLER - min*STEP_MULTIPLER);
+        seekBar.setMin(0);
         seekBar.setOnSeekBarChangeListener(new SimpleOnSeekBarChangeListener(){
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int offset = (min*step) - min;
-                currentValue = progress*step - offset;
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                currentValue = getClosestValueTo(progress);
                 setLabel();
                 notifyChange();
             }
@@ -101,6 +115,17 @@ public class DiscreteNumberPicker extends LinearLayout {
         if (listener != null) {
             listener.onNumberChanged(currentValue);
         }
+    }
+
+    private int getClosestValueTo(int value) {
+        Log.d("SEEKBAR", "value: " + value);
+        Log.d("SEEKBAR", "min:" + seekBar.getMin());
+        Log.d("SEEKBAR", "max:" + seekBar.getMax());
+        int floor = values.floor(value);
+        int ceiling = values.ceiling(value);
+        int lowerDistance = Math.abs(value - floor);
+        int higherDistance = Math.abs(value - ceiling);
+        return ((lowerDistance < higherDistance ? floor : ceiling)/STEP_MULTIPLER) + min;
     }
 
     public void setOnNumberChangedListener(OnNumberChangedListener listener) {
@@ -119,7 +144,7 @@ public class DiscreteNumberPicker extends LinearLayout {
     }
 
     private void setSeekbarValue() {
-        int seekbarValue = (currentValue + (min*step - min)) / step;
+        int seekbarValue = currentValue*STEP_MULTIPLER;
         seekBar.setProgress(seekbarValue);
     }
 
